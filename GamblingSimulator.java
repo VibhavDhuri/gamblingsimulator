abfd59be8958b6fdbfea54375707a3f83a9592e0
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-24 21:14:42 +0530

    Added: UC2

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,11 +2,23 @@ package gamblingsimulator;
 
 public class GamblingSimulator {
 	static int stake = 100;
+	static int cash = stake;
+	static int bets = 0;
+
+	static void placeBet() {
+		bets++;
+		if (Math.random() < 0.5) {
+			cash++;
+			System.out.println("Bet won.");
+		} else {
+			cash--;
+			System.out.println("Bet lost.");
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator Program!");
-		int bets = 0;
-		System.out.println("Current stake: " + stake);
+		placeBet();
 	}
 
 }
