8b7be76ab867bf386c48f1494a9d3eb2f23e0f9e
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-25 00:17:55 +0530

    Added: UC4

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -7,6 +7,8 @@ public class GamblingSimulator {
 	static int cash = stake;
 	static int bets = 0;
 	static int betsWon = 0;
+	static int profitOrLoss = 0;
+	static int totalProfitOrLoss = 0;
 
 	static void placeBet() {
 		bets++;
@@ -26,8 +28,16 @@ public class GamblingSimulator {
 		}
 		checkDailyLimit();
 		System.out.println("Stake at the end of the day is: " + cash);
+		profitOrLoss = cash - 100;
+		{
+			if (profitOrLoss == -50)
+				System.out.println("There is loss of 50.");
+			else
+				System.out.println("There is profit of 50.");
+		}
 		System.out.println("Total Bets: " + bets);
 		System.out.println("Bets Won: " + betsWon);
+		cash = 100;
 	}
 
 	static void checkDailyLimit() {
@@ -37,9 +47,23 @@ public class GamblingSimulator {
 			System.out.println("Lower limit reached! Resigning for the day...");
 	}
 
+	static void playTwentyDays() {
+		for (int i = 0; i < 2; i++) {
+			dailyPlay();
+			totalProfitOrLoss = totalProfitOrLoss + profitOrLoss;
+		}
+		if (totalProfitOrLoss < 0)
+			System.out.println("Total loss is: " + totalProfitOrLoss);
+		else if (totalProfitOrLoss == 0)
+			System.out.println("There is no profit or loss.");
+		else
+			System.out.println("The profit is: " + totalProfitOrLoss);
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator Program!");
-		dailyPlay();
+		playTwentyDays();
 	}
 
 }
